e8c7451c78782bc4568e48153bfd0364ba80d534
vanessa-schattman <ness.schattman@mail.utoronto.ca>
2024-12-02 01:30:25 -0500

    fix: fills in and debugs Food

--- app/src/main/java/com/example/b07fall2024/QuesAns.java
+++ app/src/main/java/com/example/b07fall2024/QuesAns.java
@@ -2,16 +2,17 @@ package com.example.b07fall2024;
 
 import java.util.Map;
 
+import java.util.Map;
+
 public interface QuesAns {
     String getQuestionText(int questionIndex);
 
     Map<String,String> getOptions(int questionIndex);
 
-    String getSelectedAnswer(String question);
-    void setSelectedAnswer(String question,String answer);
-    boolean isValidOption(int questionIndex,String answer);
-    int questionTextSize();
+    String getSelectedAnswer(int questionIndex);
+    void setSelectedAnswer(int index,String answer);
+    int getEnding_quiz_number();
+    int options_size(int number);
     float getEmissions();
-
 }
 
